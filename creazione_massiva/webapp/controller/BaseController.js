06adee12f3e72b72f2d17315767fe3d72b247361
Lulle91 <125670291+Lulle91@users.noreply.github.com>
2024-02-19 10:49:06 +0100

    fix columns spreadsheet

--- creazione_massiva/webapp/controller/BaseController.js
+++ creazione_massiva/webapp/controller/BaseController.js
@@ -1,133 +1,145 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History", "sap/ui/core/UIComponent",
-   "sap/ui/core/Fragment", "sap/ui/export/Spreadsheet",],
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History",
+    "sap/ui/core/UIComponent",
+    "sap/ui/core/Fragment",
+    "sap/ui/export/Spreadsheet",
+  ],
   function (Controller, History, UIComponent, Fragment, Spreadsheet) {
-      "use strict";
+    "use strict";
 
-      return Controller.extend("granterre.creazionemassiva.controller.BaseController", {
-          
-          /**
-           * Convenience method for getting the view model by name in every controller of the application.
-           * @public
-           * @param {string} sName the model name
-           * @returns {sap.ui.model.Model} the model instance
-           */
-          getModel: function (sName) {
-              return this.getView().getModel(sName);
-          },
-
-          /**
-           * Convenience method for setting the view model in every controller of the application.
-           * @public
-           * @param {sap.ui.model.Model} oModel the model instance
-           * @param {string} sName the model name
-           * @returns {sap.ui.core.mvc.View} the view instance
-           */
-          setModel: function (oModel, sName) {
-              return this.getView().setModel(oModel, sName);
-          },
-          onOpenDialog: function(dialName, fragmName, self, ...oModel) {
-            let oView = this.getView();
-            dialName = self.dialName;
-            if (!dialName) {
-                dialName = Fragment.load({
-                    id: oView.getId(),
-                    name: fragmName,
-                    controller: self,
-                }).then((oValueHelpDialog) => {             
-                    oView.addDependent(oValueHelpDialog);
-                    oValueHelpDialog.setModel(this.getModel(...oModel));
-                    return oValueHelpDialog;
-                });
-                dialName.then(function(oValueHelpDialog) {
-                    oValueHelpDialog.open();
-                });
-            }else{                
-                self.dialName.open()
-            }        
+    return Controller.extend(
+      "granterre.creazionemassiva.controller.BaseController",
+      {
+        /**
+         * Convenience method for getting the view model by name in every controller of the application.
+         * @public
+         * @param {string} sName the model name
+         * @returns {sap.ui.model.Model} the model instance
+         */
+        getModel: function (sName) {
+          return this.getView().getModel(sName);
         },
-          /**
-           * Convenience method for getting the resource bundle.
-           * @public
-           * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
-           */
-          getResourceBundle: function () {
-              return this.getOwnerComponent().getModel("i18n").getResourceBundle();
-          },
 
-          /**
-           * Method for navigation to specific view
-           * @public
-           * @param {string} psTarget Parameter containing the string for the target navigation
-           * @param {Object.<string, string>} pmParameters? Parameters for navigation
-           * @param {boolean} pbReplace? Defines if the hash should be replaced (no browser history entry) or set (browser history entry)
-           */
-          navTo: function (psTarget, pmParameters, pbReplace) {
-              this.getRouter().navTo(psTarget, pmParameters, pbReplace);
-          },
-
-          getRouter: function () {
-              return UIComponent.getRouterFor(this);
-          },
-
-          onNavBack: function () {
-            debugger
-              const sPreviousHash = History.getInstance().getPreviousHash();
-
-              if (sPreviousHash !== undefined) {
-                  window.history.back();
-              } else {
-                  this.getRouter().navTo("appHome", {}, true /* no history*/);
-              }
-          },
-          _getColumnsConfig: function(oTable) {
-            debugger
-            const aCols = [];
-            
-            oTable.getColumns().forEach((el, key) => {
-              debugger
-         
-                let property = "";
-                oTable.getItems().forEach((row, i) => {
-                  const cell = row.getCells()[key]
-                  if(cell.getMetadata().getElementName() === 'sap.ui.core.Icon') {
-                    property = "note";
-                  }else if(cell.getBindingInfo("text")){
-                    property = cell.getBindingInfo("text").parts[0].path
-                  }
-                })       
-                aCols.push({
-                    label: el.getHeader().getText(),
-                    property: property,
-                    type: String
-                });            
-              
-                 
-            });
-            return aCols;
+        /**
+         * Convenience method for setting the view model in every controller of the application.
+         * @public
+         * @param {sap.ui.model.Model} oModel the model instance
+         * @param {string} sName the model name
+         * @returns {sap.ui.core.mvc.View} the view instance
+         */
+        setModel: function (oModel, sName) {
+          return this.getView().setModel(oModel, sName);
         },
-        
-        DownloadTable: function(oEvent) {
-            debugger
-            const oTable = oEvent.getSource().getParent().getParent().getAggregation("content")
-            const oRowBinding = oTable.getBinding('items');
-            const aCols = this._getColumnsConfig(oTable);
-            const oSheet = new Spreadsheet({
-                workbook: {
-                    columns: aCols,
-                    hierarchyLevel: 'Level'
-                },
-                dataSource: oRowBinding,
-                fileName: 'List'
+        onOpenDialog: function (dialName, fragmName, self, ...oModel) {
+          let oView = this.getView();
+          dialName = self.dialName;
+          if (!dialName) {
+            dialName = Fragment.load({
+              id: oView.getId(),
+              name: fragmName,
+              controller: self,
+            }).then((oValueHelpDialog) => {
+              oView.addDependent(oValueHelpDialog);
+              oValueHelpDialog.setModel(this.getModel(...oModel));
+              return oValueHelpDialog;
             });
-            
-            oSheet.build().finally(function() {
-                oSheet.destroy();
+            dialName.then(function (oValueHelpDialog) {
+              oValueHelpDialog.open();
             });
-        },
-          onClose: function(oEvent){
-            oEvent.getSource().getParent().close()
+          } else {
+            self.dialName.open();
           }
-      });
-  },
+        },
+        /**
+         * Convenience method for getting the resource bundle.
+         * @public
+         * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
+         */
+        getResourceBundle: function () {
+          return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+        },
+
+        /**
+         * Method for navigation to specific view
+         * @public
+         * @param {string} psTarget Parameter containing the string for the target navigation
+         * @param {Object.<string, string>} pmParameters? Parameters for navigation
+         * @param {boolean} pbReplace? Defines if the hash should be replaced (no browser history entry) or set (browser history entry)
+         */
+        navTo: function (psTarget, pmParameters, pbReplace) {
+          this.getRouter().navTo(psTarget, pmParameters, pbReplace);
+        },
+
+        getRouter: function () {
+          return UIComponent.getRouterFor(this);
+        },
+
+        onNavBack: function () {
+          debugger;
+          const sPreviousHash = History.getInstance().getPreviousHash();
+
+          if (sPreviousHash !== undefined) {
+            window.history.back();
+          } else {
+            this.getRouter().navTo("appHome", {}, true /* no history*/);
+          }
+        },
+        _getColumnsConfig: function (oTable) {
+          debugger;
+          const aCols = [];
+
+          oTable.getColumns().forEach((el, key) => {
+            debugger;
+            if (key !== 0) {
+              let property = "";
+              oTable.getItems().forEach((row, i) => {
+                const cell = row.getCells()[key];
+                if (
+                  cell.getMetadata().getElementName() === "sap.ui.core.Icon"
+                ) {
+                  property = "note";
+                } else if (cell.getBindingInfo("text")) {
+                  property = cell.getBindingInfo("text").parts[0].path;
+                }
+              });
+              aCols.push({
+                label: el.getHeader().getText(),
+                property: property,
+                type: String,
+              });
+            }
+          });
+          return aCols;
+        },
+
+        DownloadTable: function (oEvent) {
+          debugger;
+          const oTable = oEvent
+            .getSource()
+            .getParent()
+            .getParent()
+            .getAggregation("content");
+          const oRowBinding = oTable.getBinding("items");
+          const aCols = this._getColumnsConfig(oTable);
+          const oSheet = new Spreadsheet({
+            workbook: {
+              columns: aCols,
+              hierarchyLevel: "Level",
+            },
+            dataSource: oRowBinding,
+            fileName: "List",
+          });
+
+          oSheet.build().finally(function () {
+            oSheet.destroy();
+          });
+        },
+        onClose: function (oEvent) {
+          oEvent.getSource().getParent().close();
+        },
+      }
+    );
+  }
 );
